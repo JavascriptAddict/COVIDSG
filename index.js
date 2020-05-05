d5a5e6d4189184b07c94f462c93a087c1c17027b
Mario Lim <44302894+JavascriptAddict@users.noreply.github.com>
2020-05-05 15:01:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function getAPK(){
+  ga('send', {
+  hitType: 'event',
+  eventCategory: 'App',
+  eventAction: 'click',
+  eventLabel: 'APK Download'
+});
   window.location.href = 'covidsg.apk';
 }
