938a75e5440773a930564cf95ba41a92c6bf8001
Mario Lim <44302894+JavascriptAddict@users.noreply.github.com>
2020-05-05 15:45:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+var clientId = ga.getAll()[0].get('clientId');
+
+ ga('create', 'UA-165610630-1', {
+        'clientId': clientId
+    });
+
 function getAPK(){
 
-ga('send', 'event', 'APK Download', 'click', {
-hitCallback: function(){
- window.location.href = 'covidsg.apk'; 
-}
-
-});
-
+ ga('send', 'event', {
+   'eventCategory': 'APK Download', //required
+   'eventAction': 'download', //required
+   'eventLabel': 'APK Download',
+   'eventValue': 1,
+   'hitCallback': function() {
+       console.log('Sent!!');
+       window.location.href = 'covidsg.apk'; 
+      //callback function
+    },
+   'hitCallbackFail' : function () {
+      console.log("Unable to send Google Analytics data");
+      //callback function
+   }
 }
